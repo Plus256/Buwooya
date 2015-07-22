327fa9edca2e6a882964608bbfcfb0df0ed2654b
Collins Wagaba <wagabaplus256@gmail.com>
2015-07-23 02:25:31 +0300

    cloned accommodation file to create other files

---
+++ inc/internship_details.php
@@ -0,0 +1,141 @@
+<?php
+?>
+<div class="details_links">
+  <a href="#" class="details_links_cat" style="background:#A6CE39; color:#FFF;" onclick="return false;">Accomodation</a>
+  <a href="#" class="details_links_cat" onclick="getDetails('fee'); return false;">Fees</a>
+  <a href="#" class="details_links_cat" onclick="getDetails('internship'); return false;">Internships</a>
+  <a href="#" class="details_links_cat" onclick="getDetails('planning'); return false;">Planning</a>
+  <div class="spacer"></div>
+</div>
+<div class="details_cat_body">
+
+  <div class="details_entry">
+    <div class="details_entry_thumb">
+      <img src="img/acc_one.jpg" />
+    </div>
+    <div class="details_entry_caption">
+      <div class="details_entry_caption_title">
+        The House
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          During village trips accommodation may range from simple guesthouses to mattresses on the floor of the church to camping in tents.
+          Be prepared to have no electricity, to shower with a bucket of water or baby wipes, and to use a squatty potty! (hole-in-the-ground toilet).
+        </p>
+        <p>
+          All volunteers and teams will stay at Buwooya Child Care Ministries guesthouse in Buwooya Village.
+          The house is  secure and westernized.
+        </p>
+        <p>
+          Even though the house is westernized, Buwooya is still an African Village, with no electricity, has solar power and some times no running water.
+        </p>
+        <p>
+          The atmosphere at the guesthouses is very community-oriented. You'll be living with other international volunteers and a few of Buwooya Child Care Ministries Ugandan  staff members, giving you a chance to get to know the team very well.
+        <br />You will likely be sharing a room with other volunteers of the same sex.
+        </p>
+      </div>
+      <div class="spacer"></div>
+    </div>
+    <div class="spacer"></div>
+  </div>
+
+  <div class="details_entry">
+    <div class="details_entry_thumb">
+      <div class="details_entry_caption_title">
+        Drinking Water
+      </div>
+      <div class="details_entry_caption_body">
+        <p>Drinking water from the tap is not recommended. The guesthouse boils water to kill any germs and bacteria.
+        Each volunteer will take turns boiling water in the evenings to ensure there is enough drinking water the next day.<br />You may also purchase your own bottled water (at your own expense) if you choose not to drink the water provided by the guesthouse.</p>
+      </div>
+      <div class="details_entry_caption_title">
+        Showering
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          There is running water in the Buwooya Child Care guesthouse. As long as showers are spaced apart long enough, And on occasion  we have to fetch water from a nearby well. In these times you will have to shower with a cup and basin of water for a few days.
+        </p>
+        <p>
+          When staying in the village there is no running water and you will be using a cup and basin of water to shower. Shower stalls are usually outside and made up of a brick wall enclosure that may or may not have a roof. Warm water may not be available. Again, please be flexible in the village.
+        </p>
+      </div>
+      <div class="details_entry_caption_title">
+        Washing Clothes
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          There are a few options for washing clothes. You may go to a laundromat near the house, give your clothes to our housekeeper who will wash clothes once a week for a small fee (usually $2-$3), or hand wash them yourself.<br />
+          Please provide your own laundry soap. This can be bought at a supermarket close to the guesthouse or you can certainly bring some.
+        </p>
+      </div>
+      <div class="spacer"></div>
+    </div>
+    <div class="details_entry_caption">
+      <img src="img/acc_four.jpg" />
+    </div>
+    <div class="spacer"></div>
+  </div>
+
+  <div class="details_entry">
+    <div class="details_entry_thumb">
+      <img src="img/acc_three.jpg" />
+    </div>
+    <div class="details_entry_caption">
+      <div class="details_entry_caption_title">
+        Food
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          Breakfast usually consists of toast and tea. During the weekdays, lunches are prepared for the entire team. The normal lunches are beans and rice. The international volunteers and staff that live in the guesthouse take turns cooking dinner.
+        </p>
+        <p>
+          Dinners can range from the typical Ugandan  meal to simple American meals such as spaghetti. The typical Ugandan meal is meat or beans mixed and fried with vegetables, rice, matooke (a boiled plantain) posho/ugali (mixed cornmeal), and potatoes.<br />Fruits, such as pineapple and bananas are typically around the house as a snack.
+        </p>
+        <p>
+          We advise you to bring some spending money for westernized meals outside the guesthouse. There are also westernized grocery stores that sell westernized items. You should also consider bringing snacks from home: peanut butter, beef jerky, trail mix, cookies, crackers, chips, granola bars, and power bars.<br />These are third world countries and do not have the wide variety of snacks that you may be used to.
+        </p>
+      </div>
+      <div class="spacer"></div>
+    </div>
+    <div class="spacer"></div>
+  </div>
+
+  <div class="details_entry">
+    <div class="details_entry_thumb">
+      <div class="details_entry_caption_title">
+        Wi-Fi
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          Buwooya Child Care  guesthouses in  Uganda has no wireless Internet access, although we have internet modem . This will allow you to stay in touch with loved ones at home. There is no access to Wi-Fi in the village.
+        </p>
+      </div>
+      <div class="details_entry_caption_title">
+        Cell Phones
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          You can certainly bring a cell phone if it takes a SIM card. Once you get to your destination you can buy a SIM card with a Ugandan  number for about $5. There is also the option of buying a phone in Uganda for $40.<br />Some long term volunteers do this to communicate with staff while in Africa, however most download a phone app or Skype to communicate with friends and family.
+        </p>
+      </div>
+      <div class="details_entry_caption_title">
+        Mail/Postal Services
+      </div>
+      <div class="details_entry_caption_body">
+        <p>
+          If someone wants to send you a package please allow at least 4 – 8 weeks for it to arrive from the US. Your friends and family can send mail to you at:
+        </p>
+        <b>Buwooya Child Care Ministries, P. O. Box 1100 Lugazi, Uganda</b>
+      </div>
+      <div class="spacer"></div>
+    </div>
+    <div class="details_entry_caption">
+      <img src="img/acc_two.jpg" />
+    </div>
+    <div class="spacer"></div>
+  </div>
+
+</div>
+<div class="spacer"></div>
+<?php
+?>
